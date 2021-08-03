8542178e012f9efb551a5db975509d8b8448f673
Mertcan baş <mertcanbas35@gmail.com>
2021-08-03 14:30:28 +0300

    first commit

---
+++ src/screens/auth/InitialScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const InitialScreen = () => {
+  return (
+    <View>
+      <Text>InitialScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default InitialScreen;
